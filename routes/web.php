70a90c645afe94926ba2eb96c64432e99822b3b8
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2022-08-16 23:08:50 +0100

    add route for dashboards

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware'=>['auth']], function(){
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
