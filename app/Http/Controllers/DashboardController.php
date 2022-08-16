0872960501a594fffc45550b4141091e6b3be51e
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2022-08-16 22:27:30 +0100

    add dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Auth;
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+    {
+        if(auth::user()->hasRole('user'))
+        {
+            return view('userdash');
+        }
+        elseif(auth::user()->hasRole('blogwriter'))
+        {
+            return view('blogwriter_dash');
+        }
+        elseif(auth::user()->hasRole('admin'))
+        {
+            return view('dashboard');
+        }
+
+    }
+}
